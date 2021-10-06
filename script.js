1f65cf1d2779a7d298558297d57b91523f1504c3
Dauphine Burns <dauphineburns@gmail.com>
2021-10-06 13:42:36 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -14,25 +14,19 @@
 $(document).ready(function(){
   
     function freshDot(){
-      this.obj = document.createElement("div");
-      this.obj.classList.add("box");
-      this.obj.style.top = (window.innerHeight * Math.random()) + 'px';
-      this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
+      this.images = document.createElement("div");
+      this.images.classList.add("box");
+      this.images.style.top = (window.innerHeight * Math.random()) + 'px';
+      this.images.style.left = (window.innerWidth * Math.random()) + 'px';
       this.size = Math.floor(5 * Math.random()) + 3;
-      this.obj.style.height =  this.size + 'px';
-      this.obj.style.width = this.size + 'px';
+      this.images.style.height =  this.size + 'px';
+      this.images.style.width = this.size + 'px';
       
-      document.body.appendChild(this.obj);
+      document.body.appendChild(this.images);
     }
     var dot = [];
     for(var i = 0 ; i < 300; i++ ){
       dot.push(new freshDot());
     }
-    /*
-    $(window).resize(function(){
-      for(i=0;i<200;i++){
-        document.body.removeChild(dot[i]);
-      }
-    });
-    */
+    
   });
